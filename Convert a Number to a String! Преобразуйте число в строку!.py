4779fb2fc1126a264ebb6fc6099951696f48cd4f
proven_ak <proven@ya.ru>
2024-12-14 21:19:33 +0300

    Convert a Number to a String! Преобразуйте число в строку!

---
+++ Convert a Number to a String! Преобразуйте число в строку!.py
@@ -0,0 +1,27 @@
+# Description:
+# We need a function that can transform a number (integer) into a string.
+#
+# What ways of achieving this do you know?
+#
+# Examples (input --> output):
+# 123  --> "123"
+# 999  --> "999"
+# -100 --> "-100"
+#
+# Описание:
+# Нам нужна функция, которая может преобразовывать число (integer) в строку.
+#
+# Какие способы достижения этой цели вы знаете?
+#
+# Примеры (ввод -> вывод):
+# 123  --> "123"
+# 999  --> "999"
+# -100 --> "-100"
+
+
+def number_to_string(num):
+
+    return str(num)
+
+print(number_to_string(67), '67')
+print(number_to_string(79585), '79585')
